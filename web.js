2a5e2159d3919b946814e802c4708f0695768b9d
astaric <anze.staric@gmail.com>
2013-07-04 22:15:43 +0200

    Fixed encoding parameter.

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var template = fs.readFileSync('index.html', {"encoding": "utf-8"});
+var template = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
   response.send(template);
